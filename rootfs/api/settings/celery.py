48d9ebe74af0d18965b39db5b6ab914ec106ad09
duanhongyi <duanhongyi@doopai.com>
2023-11-08 15:32:59 +0800

    fix(controller): rabbitmq sharding err

--- rootfs/api/settings/celery.py
+++ rootfs/api/settings/celery.py
@@ -19,7 +19,9 @@ class Config:
     result_expires = 24 * 60 * 60
     broker_url = os.environ.get('DRYCC_RABBITMQ_URL', 'amqp://guest:guest@127.0.0.1:5672/')  # noqa
     cache_backend = 'django-cache'
-    task_default_queue = 'controller.priority.low'
+    task_default_queue = 'controller'
+    task_default_exchange = 'priority'
+    task_default_routing_key = 'controller.priority.low'
     worker_cancel_long_running_tasks_on_connection_loss = True
 
 
@@ -27,10 +29,26 @@ app = Celery('drycc')
 app.config_from_object(Config)
 app.conf.update(
     task_routes={
-        'api.tasks.scale_app': {'queue': 'controller.priority.high'},
-        'api.tasks.restart_app': {'queue': 'controller.priority.high'},
-        'api.tasks.retrieve_resource': {'queue': 'controller.priority.high'},
-        'api.tasks.send_measurements': {'queue': 'controller.priority.middle'},
+        'api.tasks.scale_app': {
+            'queue': 'priority',
+            'exchange': 'controller',
+            'routing_key': 'controller.priority.high',
+        },
+        'api.tasks.restart_app': {
+            'queue': 'priority',
+            'exchange': 'controller',
+            'routing_key': 'controller.priority.high',
+        },
+        'api.tasks.retrieve_resource': {
+            'queue': 'priority',
+            'exchange': 'controller',
+            'routing_key': 'controller.priority.high',
+        },
+        'api.tasks.send_measurements': {
+            'queue': 'priority',
+            'exchange': 'controller',
+            'routing_key': 'controller.priority.middle',
+        },
     },
 )
 app.autodiscover_tasks()
