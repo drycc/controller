478cd6fdbb0e221e85e8e6b40641e54f7ce1a00f
duanhongyi <duanhongyi@doopai.com>
2025-04-14 17:29:21 +0800

    fix(migrate): group migrate error

--- rootfs/api/migrations/0021_migration_dryccfile.py
+++ rootfs/api/migrations/0021_migration_dryccfile.py
@@ -9,28 +9,16 @@ def migration_dryccfile(apps, schema_editor):
         build = release.build
         if not build or not build.dryccfile or "pipeline" in build.dryccfile:
             continue
-        config, envs = {}, {}
-        if release.config:
-            for value in release.config.values:
-                if "ptype" in value:
-                    if value["ptype"] not in envs:
-                        envs[value["ptype"]] = {}
-                    envs[value["ptype"]][value["name"]] = value["value"]
-                elif "group" in value:
-                    if value["group"] not in envs:
-                        config[value["group"]] = {}
-                    config[value["group"]][value["name"]] = value["value"]
         pipeline = {}
         for key, value in build.dryccfile.get('deploy', {}).items():
             pipeline.update({
                 "%s.yaml" % key: {
                     "kind": "pipeline",
                     "ptype": key,
-                    "env": envs.get(key, {}),
                     "deploy": value,
                 }
             })
-        build.dryccfile = {"config": config, "pipeline": pipeline}
+        build.dryccfile = {"pipeline": pipeline}
         build.save()
 
 
